f83d7672ff587d1eb91320dda8061e120f07ce30
lurkny <larsonbm@colostate.edu>
2024-12-18 10:27:11 -0700

    silenced cargo build output in scripts

--- scripts/offset_tracking_demo.sh
+++ scripts/offset_tracking_demo.sh
@@ -5,20 +5,20 @@ echo "Starting Rafka offset tracking demo..."
 
 # Start broker
 echo "Starting broker..."
-cargo run --bin start_broker -- --port 50051 &
+cargo run -q --bin start_broker -- --port 50051 &
 BROKER_PID=$!
 sleep 2
 
 # Start first consumer
 echo "Starting first consumer..."
-cargo run --bin start_consumer -- --port 50051 &
+cargo run -q --bin start_consumer -- --port 50051 &
 CONSUMER1_PID=$!
 sleep 2
 
 # Send initial batch of messages
 echo "Sending first batch of messages..."
 for i in {1..3}; do
-    cargo run --bin start_producer -- \
+    cargo run -q --bin start_producer -- \
         --port 50051 \
         --message "Batch1-Message$i" \
         --key "key-$i"
@@ -34,14 +34,14 @@ kill $CONSUMER1_PID
 sleep 2
 
 echo "Starting second consumer with same ID..."
-cargo run --bin start_consumer -- --port 50051 &
+cargo run -q --bin start_consumer -- --port 50051 &
 CONSUMER2_PID=$!
 sleep 2
 
 # Send second batch of messages
 echo "Sending second batch of messages..."
 for i in {4..6}; do
-    cargo run --bin start_producer -- \
+    cargo run -q --bin start_producer -- \
         --port 50051 \
         --message "Batch2-Message$i" \
         --key "key-$i"
