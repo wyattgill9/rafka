1b7e587ff5ff81f112f0621b837e725c45e45283
RaoniSilvestre <raoni.silvestre@hotmail.com>
2024-12-18 21:01:38 -0300

    feat: Creating basic structure of cli

---
+++ crates/cli/src/lib.rs
@@ -0,0 +1,52 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct CLI {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    Broker {
+        #[arg(short, long, default_value = "50051")]
+        port: u16,
+
+        #[arg(long, default_value = "0")]
+        partition: u32,
+
+        #[arg(short, long, default_value = "1")]
+        total_partition: u32,
+
+        #[arg(short, long, default_value = "1")]
+        retention_secs: u64,
+    },
+    Consumer {
+        #[arg(short, long, default_value = "50051")]
+        port: u16,
+
+        #[arg(long, default_value = "0")]
+        partition: u32,
+    },
+    Producer {
+        #[arg(short, long, default_value = "127.0.0.1:50051")]
+        brokers: Vec<String>,
+
+        #[arg(short, long, default_value = "Hello, world!")]
+        message: String,
+
+        #[arg(short, long, default_value = "default-key")]
+        key: String,
+    },
+    Metrics {
+        #[arg(short, long, default_value = "50051")]
+        port: u16,
+    },
+}
+
+impl CLI {
+    pub fn get_parse() -> Commands {
+        CLI::parse().command
+    }
+}
