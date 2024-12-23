da04da212e14d8b689da2407c1a95eb2f1596b4c
Shidowy <101477459+Shidowy@users.noreply.github.com>
2024-12-22 16:31:05 -0800

    moved shell files

--- scripts/helloworld.sh
+++ scripts/helloworld.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
-set -e
 
-echo "Starting Rafka demo..."
-
-# Start broker
-echo "Starting broker..."
-cargo run --bin start_broker &
+# Start the broker in the background
+echo "Starting the broker..."
+RUST_LOG=info cargo run --bin broker & 
 BROKER_PID=$!
-sleep 2
+sleep 2 # Give the broker some time to start
 
-# Start consumer
-echo "Starting consumer..."
-cargo run --bin start_consumer &
+# Start the producer and publish a message
+echo "Testing the producer..."
+RUST_LOG=info cargo run --bin producer -- "127.0.0.1:8080" "test_topic" "key1" "Hello, Rafka!" &
+PRODUCER_PID=$!
+sleep 2 # Allow some time for the producer to run
+
+# Start the consumer and attempt to consume the message
+echo "Testing the consumer..."
+RUST_LOG=info cargo run --bin consumer -- "127.0.0.1:8080" "test_topic" &
 CONSUMER_PID=$!
-sleep 2
+sleep 5 # Allow some time for the consumer to run
 
-# Run producer
-echo "Sending message..."
-cargo run --bin start_producer "Hello, World!"
-sleep 1
+# Kill all background processes
+echo "Cleaning up..."
+kill $BROKER_PID $PRODUCER_PID $CONSUMER_PID
+wait $BROKER_PID $PRODUCER_PID $CONSUMER_PID 2>/dev/null
 
-# Clean up
-kill $BROKER_PID $CONSUMER_PID 2>/dev/null || true
-wait
-./scripts/kill.sh
-
-echo "Demo completed"
+echo "Test complete."
